83c5ecbe44499a0938bf48f414795ab33a261e77
Takesure Mamvota <tooyoungmamvota@gmail.com>
2022-03-15 23:57:47 +0700

    Client signup

---
+++ client/pages/_app.js
@@ -0,0 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default ({ Component, pagePops }) => {
+  return <Component {...pagePops} />;
+};
---
+++ client/pages/auth/signup.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import useRequest from "../../hooks/use-request";
+
+export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit} className="container">
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+          type="email"
+          placeholder="Enter Email"
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="form-control"
+          type="password"
+          placeholder="Enter Password"
+        />
+      </div>
+
+      {errors}
+
+      <button type="submit" className="btn btn-primary my-3">
+        Sign Up
+      </button>
+    </form>
+  );
+};
